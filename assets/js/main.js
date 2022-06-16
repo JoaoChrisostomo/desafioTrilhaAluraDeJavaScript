ba71162a26e3e08f7681f335a35cc1b29e95512f
Joao Chrisostomo <maturandomassas@gmail.com>
2022-06-16 18:34:44 -0300

    utilizando a biblioteca moment.js para formatar o tipo de data que eu gostaria de pegar e utilizando no projeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,39 @@
 import BotaoConclui from '../components/concluirTarefa.js'
 import BotaoDeleta from '../components/deletaTarefa.js'
 
-const criarTarefa = evento => {
+const handleNovoItem = evento => {
   evento.preventDefault()
 
   const lista = document.querySelector('[data-list]')
   const input = document.querySelector('[data-form-input]')
   const valorInput = input.value
 
-  const tarefa = document.createElement('li')
-  tarefa.classList.add('task')
-  const conteudo = `<p class="content">${valorInput}</p>`
+  const calendario = document.querySelector('[data-form-date]')
+  const data = moment(calendario.value)
+  const dataFormatada = data.format('DD/MM/YYYY')
 
-  tarefa.innerHTML = conteudo
+  const dados = {
+    valorInput,
+    dataFormatada
+  }
 
-  tarefa.appendChild(BotaoConclui())
-  tarefa.appendChild(BotaoDeleta())
-  lista.appendChild(tarefa)
+  const criaTarefa = criarTarefa(dados)
+
+  lista.appendChild(criaTarefa)
   input.value = ' '
 }
 
+const criarTarefa = ({ valorInput, dataFormatada }) => {
+  const tarefa = document.createElement('li')
+  tarefa.classList.add('task')
+  const conteudo = `<p class="content">${dataFormatada} * ${valorInput}</p>`
+
+  tarefa.innerHTML = conteudo
+  tarefa.appendChild(BotaoConclui())
+  tarefa.appendChild(BotaoDeleta())
+
+  return tarefa
+}
+
 const novaTarefa = document.querySelector('[data-form-button]')
-novaTarefa.addEventListener('click', criarTarefa)
+novaTarefa.addEventListener('click', handleNovoItem)
